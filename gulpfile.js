697719231b23ab8c28e1993bdcf0cae73761521a
Denilson Ribeiro <denilsonribeiro@hotmail.com.br>
2019-03-23 15:35:32 -0300

    Rodapé finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,50 @@
-const {src, dest, task, watch} = require('gulp');
+const {src, dest, task, watch, series} = require('gulp');
 const gulpSass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload();
 
 function sass(){
     return src('src/sass/**/*.scss')
-            .pipe(gulpSass())
-            .pipe(dest('src/css/'))
+        .pipe(gulpSass())
+        .pipe(dest('src/css/'))
 }
 
-function watch_files(done){
-    watch('src/sass/**/*.scss', sass);
+function html(){
+    return src('src/**/*')
+        .pipe(dest('src'))
+}
+
+//Código comentado retirar /* do caminho
+/*function watch_files(done){
+    watch('src/sass/**//*/*.scss', sass);
     
+    done();
+}*/
+
+
+function serve(done){
+    browserSync.init({
+        server:{
+            baseDir: 'src',
+        }
+    });
+    watch('src/sass/**/*.scss', sass);
+    watch('src/css/').on('change', browserSync.reload);
     done();
 }
 
-task(sass);
-task(watch_files);
+//task(sass);
+//task(watch_files);
+
+exports.html = html;
+exports.sass = sass;
+exports.serve = serve;
+
+var build = series(html, sass, serve);
+var syncWeb = build;
+task('default', syncWeb);
+
+
+
+
+
