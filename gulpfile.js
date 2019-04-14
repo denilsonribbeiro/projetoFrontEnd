eb316c62f96fcda394fa89082718ee2c083d982c
Denilson Ribeiro <denilsonribeiro@hotmail.com.br>
2019-04-13 21:07:03 -0300

    Menu com acessibilidade concluído

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 const {src, dest, task, watch, series} = require('gulp');
 const gulpSass = require('gulp-sass');
+const include = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload();
+
+
+function copy(){
+    return src(['src/components/**/*', 'src/css/**/*', 'src/javascript/**/*', 'src/imagens/**/*'], {'base': 'src'})
+        .pipe(dest('dist/'))
+}
 
 function sass(){
     return src('src/sass/**/*.scss')
         .pipe(gulpSass())
-        .pipe(dest('src/css/'))
+        .pipe(dest('dist/css/'))
 }
 
 function html(){
-    return src('src/**/*')
-        .pipe(dest('src'))
+    return src('src/index.html')
+        .pipe(include())
+        .pipe(dest('dist/'))
 }
 
 //Código comentado retirar /* do caminho
@@ -25,22 +33,24 @@ function html(){
 function serve(done){
     browserSync.init({
         server:{
-            baseDir: 'src',
+            baseDir: 'dist',
         }
     });
-    watch('src/sass/**/*.scss', sass);
-    watch('src/css/').on('change', browserSync.reload);
+    watch('src/', sass);
+    watch('src/**/*.html', html)
+    watch('dist/**/*').on('change', browserSync.reload);
     done();
 }
 
 //task(sass);
 //task(watch_files);
 
+exports.copy = copy;
 exports.html = html;
 exports.sass = sass;
 exports.serve = serve;
 
-var build = series(html, sass, serve);
+var build = series(copy, html, sass, serve);
 var syncWeb = build;
 task('default', syncWeb);
 
