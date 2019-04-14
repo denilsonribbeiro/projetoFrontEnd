b16e3b2c12def1a7ca9f452a7c1edf813829b9ec
Denilson Ribeiro <denilsonribeiro@hotmail.com.br>
2019-04-14 16:29:53 -0300

    Projeto finalizado, com inclusão de icones svg no rodapé, e plugins do gulp para otimizar o desempenho e monitoramento

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 const {src, dest, task, watch, series} = require('gulp');
 const gulpSass = require('gulp-sass');
 const include = require('gulp-file-include');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload();
 
 
 function copy(){
-    return src(['src/components/**/*', 'src/css/**/*', 'src/javascript/**/*', 'src/imagens/**/*'], {'base': 'src'})
+    return src(['src/components/**/*', 'src/css/**/*', 'src/imagens/**/*'], {'base': 'src'})
         .pipe(dest('dist/'))
 }
 
 function sass(){
     return src('src/sass/**/*.scss')
         .pipe(gulpSass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(cssnano())
         .pipe(dest('dist/css/'))
 }
 
@@ -22,13 +30,24 @@ function html(){
         .pipe(dest('dist/'))
 }
 
-//Código comentado retirar /* do caminho
-/*function watch_files(done){
-    watch('src/sass/**//*/*.scss', sass);
-    
-    done();
-}*/
+function css(){
+    return src('dist/components/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(dest('dist/components/'))
+}
 
+function js(){
+    return src('src/javascript/**/*')
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/javascript/'))
+}
+
+function imgmin(){
+    return src('src/imagens/*')
+        .pipe(imagemin())
+        .pipe(dest('dist/imagens/'))
+}
 
 function serve(done){
     browserSync.init({
@@ -38,6 +57,9 @@ function serve(done){
     });
     watch('src/', sass);
     watch('src/**/*.html', html)
+    watch('src/css/**/*.css', css)
+    watch('src/javascript/**/*', js)
+    watch('src/imagens/**/*', imgmin)
     watch('dist/**/*').on('change', browserSync.reload);
     done();
 }
@@ -48,9 +70,12 @@ function serve(done){
 exports.copy = copy;
 exports.html = html;
 exports.sass = sass;
+exports.css = css;
+exports.js = js;
+exports.imgmin = imgmin;
 exports.serve = serve;
 
-var build = series(copy, html, sass, serve);
+var build = series(copy, html, sass, css, js, imgmin, serve);
 var syncWeb = build;
 task('default', syncWeb);
 
